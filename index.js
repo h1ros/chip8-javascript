70a4cb47c83d9b9cec82d09c64778f5967796f5a
h1ros <data.h1ros@gmail.com>
2022-07-30 18:04:10 -0700

    Make more roms avaialble from web

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function selectRom() {
     let select = document.getElementById("select-rom")
     let rom_game = './games/' + select.options[select.selectedIndex].value + '.ch8'
     console.log('loading rom:', rom_game)
+    console.log('this within selectRom:', this)
     window.chip8.init(rom_game);
-
 }
-document.querySelector('select').addEventListener('change', selectRom)
+document.querySelector('select').addEventListener('change', selectRom.bind(this))
